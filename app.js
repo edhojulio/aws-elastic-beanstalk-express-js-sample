c5e913e551d00d4356e2d7bb7fbebf2f4eaf811e
edhojulio <edhojulio.ej@gmail.com>
2020-10-04 15:08:38 +0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Edho'));
+app.get('/', (req, res) => res.send('Hello World! This is Edho. Welcome to my first AWS project'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
