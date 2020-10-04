aa90b72cc01307aa5333440ed93bd275b4909a50
edhojulio <edhojulio.ej@gmail.com>
2020-10-04 15:58:45 +0700

    Correcting the code, add new line.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Edho. Welcome to my first AWS project\nIm testing my review stage in AWS pipeline'));
+app.get('/', (req, res) => res.send('Hello World! This is Edho. \r\n Welcome to my first AWS project. \r\n Im testing my review stage in AWS pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
