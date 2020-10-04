8f55e4e18e9a4771089fb6f4db4bf9d01acbccf3
edhojulio <edhojulio.ej@gmail.com>
2020-10-04 15:48:07 +0700

    Add review stage in pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! This is Edho. Welcome to my first AWS project'));
+app.get('/', (req, res) => res.send('Hello World! This is Edho. Welcome to my first AWS project\GIm testing my review stage in AWS pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
